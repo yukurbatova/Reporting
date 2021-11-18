4bc05cc5d3226725cda7c21fad84117c6f789b1e
unknown <yulian.sebastian@mail.ru>
2021-11-18 09:44:22 +0300

    shouldGenerateTestDataFailedTest removed

--- src/test/java/ru/netology/test/DeliveryCardTest.java
+++ src/test/java/ru/netology/test/DeliveryCardTest.java
@@ -54,22 +54,4 @@ public class DeliveryCardTest {
         $(".notification__content").shouldHave(exactText("Встреча успешно запланирована на " + generateDate(5)));
     }
 
-    @Test
-    void shouldGenerateTestDataFailedTest() {
-        $("[data-test-id=city] .input__control").setValue(generateCity());
-        $("[data-test-id=date] [type=tel]").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] [type=tel]").setValue(generateDate(2));
-        $("[data-test-id=name] .input__control").setValue(info.getName());
-        $("[data-test-id=phone] .input__control").setValue(info.getPhone());
-        $("[data-test-id=agreement] .checkbox__box").click();
-        $(byText("Запланировать")).click();
-        $(".notification__content").shouldHave(exactText("Встреча успешно запланирована на " + generateDate(3)));
-        $("[data-test-id=success-notification] .icon-button__content").click();
-        $("[data-test-id=date] [type=tel]").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id=date] [type=tel]").setValue(generateDate(5));
-        $(byText("Запланировать")).click();
-        $("[data-test-id=replan-notification] .notification__title").shouldHave(exactText("Необходимо подтверждение"));
-        $(byText("Перепланировать")).click();
-        $(".notification__content").shouldHave(exactText("Встреча успешно запланирована на " + generateDate(5)));
-    }
 }
